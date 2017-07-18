9dcf9ecdef405bb8bf32b99d12628edb33d35ad5
rdgoite <rdgoite@gmail.com>
2017-07-18 20:43:44 +0800

    Set up code for parsing String input into AccessionNumber.

---
+++ src/main/java/uk/ac/ebi/demo/accessionnumbers/AccessionNumber.java
@@ -0,0 +1,9 @@
+package uk.ac.ebi.demo.accessionnumbers;
+
+public class AccessionNumber {
+
+    public static AccessionNumber parse(String input) {
+        return new AccessionNumber();
+    }
+
+}
---
+++ src/test/java/uk/ac/ebi/demo/accessionnumbers/AccessionNumberTest.java
@@ -0,0 +1,21 @@
+package uk.ac.ebi.demo.accessionnumbers;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AccessionNumberTest {
+
+    @Test
+    public void testParse() {
+        //given:
+        String validInput = "ERR000111";
+
+        //when:
+        AccessionNumber accessionNumber = AccessionNumber.parse(validInput);
+
+        //then:
+        assertThat(accessionNumber).isNotNull();
+    }
+
+}
