cae9df943477b2fe20b43f0830a75f718be9a281
rdgoite <rdgoite@gmail.com>
2017-07-18 21:03:44 +0800

    Added test condition for successful parsing.

--- src/test/java/uk/ac/ebi/demo/accessionnumbers/AccessionNumberTest.java
+++ src/test/java/uk/ac/ebi/demo/accessionnumbers/AccessionNumberTest.java
@@ -9,16 +9,22 @@ public class AccessionNumberTest {
     @Test
     public void testParse() {
         //given:
-        String validInput = "ERR000111";
+        String validInput1 = "ERR000111";
+        String validInput2 = "A00001";
 
         //when:
-        AccessionNumber accessionNumber = AccessionNumber.parse(validInput);
+        AccessionNumber accessionNumber1 = AccessionNumber.parse(validInput1);
+        AccessionNumber accessionNumber2 = AccessionNumber.parse(validInput2);
 
         //then:
-        assertThat(accessionNumber)
-                .isNotNull()
+        assertThat(accessionNumber1)
                 .extracting("code", "number")
                 .containsExactly("ERR", "000111");
+
+        //and:
+        assertThat(accessionNumber2)
+                .extracting("code", "number")
+                .containsExactly("A", "00001");
     }
 
 }
