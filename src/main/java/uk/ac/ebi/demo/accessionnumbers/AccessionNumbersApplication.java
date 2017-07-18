7faa892a1e0a2d4b0c9477fa1fe6a5eacf5f6939
rdgoite <rdgoite@gmail.com>
2017-07-18 20:28:59 +0800

    Created project for implementing accession numbers.

---
+++ src/main/java/uk/ac/ebi/demo/accessionnumbers/AccessionNumbersApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.demo.accessionnumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessionNumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessionNumbersApplication.class, args);
+	}
+}
