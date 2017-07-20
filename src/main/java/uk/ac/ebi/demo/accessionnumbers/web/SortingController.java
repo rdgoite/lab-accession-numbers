614a04705837df1b8e686327c343862703a8e078
rdgoite <rdgoite@gmail.com>
2017-07-21 02:18:53 +0800

    Added code to process request.

--- src/main/java/uk/ac/ebi/demo/accessionnumbers/web/SortingController.java
+++ src/main/java/uk/ac/ebi/demo/accessionnumbers/web/SortingController.java
@@ -1,16 +1,31 @@
 package uk.ac.ebi.demo.accessionnumbers.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.ac.ebi.demo.accessionnumbers.AccessionNumber;
+import uk.ac.ebi.demo.accessionnumbers.Sorter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/sorter")
 public class SortingController {
 
-    @PostMapping
-    public void sort() {
+    @Autowired
+    private Sorter sorter;
 
+    @PostMapping
+    public void sort(@RequestBody String request) {
+        String[] data = request.split(",");
+        List<AccessionNumber> inputs = Arrays.stream(data)
+                .map(AccessionNumber::parse)
+                .collect(Collectors.toList());
+        sorter.sort(inputs);
     }
 
 }
--- src/test/java/uk/ac/ebi/demo/accessionnumbers/web/SortingControllerTest.java
+++ src/test/java/uk/ac/ebi/demo/accessionnumbers/web/SortingControllerTest.java
@@ -2,18 +2,27 @@ package uk.ac.ebi.demo.accessionnumbers.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import uk.ac.ebi.demo.accessionnumbers.AccessionNumber;
+import uk.ac.ebi.demo.accessionnumbers.Sorter;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @RunWith(SpringRunner.class)
@@ -22,6 +31,9 @@ public class SortingControllerTest {
 
     @Autowired
     private MockMvc webApp;
+
+    @MockBean
+    private Sorter sorter;
 
     @Test
     public void testSort() throws Exception {
@@ -38,6 +50,14 @@ public class SortingControllerTest {
 
         //then:
         assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        //and:
+        ArgumentCaptor<List<AccessionNumber>> inputCaptor = forClass((Class) List.class);
+        verify(sorter).sort(inputCaptor.capture());
+        assertThat(inputCaptor.getValue())
+                .extracting("code", "number")
+                .containsExactly(tuple("A", "00000"), tuple("ERR", "000112"),
+                        tuple("ERR", "000113"), tuple("SRR", "211001"));
     }
 
 }
